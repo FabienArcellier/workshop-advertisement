96d3fb6f23a881f3501602f37a3e21de9a89cfd9
Fabien Arcellier <fabien.arcellier@gmail.com>
2020-08-30 20:49:17 +0200

    * fix test

--- app_tests/acceptances/test_webapp.py
+++ app_tests/acceptances/test_webapp.py
@@ -7,10 +7,10 @@ class WebappTest(unittest.TestCase):
     def setUp(self):
         self.client = app.webapp.app.test_client()
 
-    def test_root_should_display_hello_world(self):
+    def test_root_should_display_OK(self):
         # Assign
         # Acts
         rv = self.client.get('/')
 
         # Assert
-        self.assertTrue(b'Hello Fabien!' in rv.data, rv.data)
+        self.assertTrue(b'OK' in rv.data, rv.data)
